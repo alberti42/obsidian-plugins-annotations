052dccafded614c75b81f665d2be12f0f2a01d3d
Andrea Alberti <a.alberti82@gmail.com>
2024-07-03 19:46:20 +0200

    Shortened the arc.

--- src/main.ts
+++ src/main.ts
@@ -354,7 +354,7 @@ export default class PluginsAnnotations extends Plugin {
 			const svg_unlocked = '<svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" \
 					fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-lock-open">\
 					<rect width="18" height="11" x="3" y="11" rx="2" ry="2"/>\
-					<path d="M7 11V7a5 5 0 0 1 9.9-1"/>\
+					 <path d="M7 11v-4c0-2.8 2.2-5 5-5 1.6 0 3.1.8 4 2"/> \
 				</svg>';
 			const svg_locked ='<svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" \
 					fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-lock">\
